a214c235cf1bea9a3c49bebdb7ddb07867761e28
appbakers <greeny100@gmail.com>
2017-01-18 12:41:06 +0900

    *auto _scripts/

--- _scripts/_vagrant_guest_websc_prebuild.bat
+++ _scripts/_vagrant_guest_websc_prebuild.bat
@@ -4,5 +4,6 @@
 
 @call _vagrant_guest_inst_vbox_guest_additions.ps1
 
-###==> installing GoogleChrome firefox jdk7
-start cmd.exe /c choco install -y GoogleChrome & start cmd.exe /c choco install -y firefox & start cmd.exe /c choco install -y jdk7
+###==> installing GoogleChrome firefox jdk7 ie11
+start cmd.exe /c choco install -y GoogleChrome & start cmd.exe /c choco install -y firefox & start cmd.exe /c choco install -y jdk7 & start cmd.exe /c choco install -y ie11
+
