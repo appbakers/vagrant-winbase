6baa1ab0337a99e925f2960043f50292253d03ce
appbakers <greeny100@gmail.com>
2017-01-19 16:45:39 +0900

    *auto _scripts/

--- _scripts/_vagrant_guest_websc_prebuild.bat
+++ _scripts/_vagrant_guest_websc_prebuild.bat
@@ -5,9 +5,5 @@
 @call _vagrant_guest_inst_vbox_guest_additions.ps1
 
 ###==> installing GoogleChrome firefox jdk7 ie11
-for /f "delim=," %%a IN ("GoogleChrome,firefox,jdk7,ie11,startmenu8,notepadplusplus,notepad2-mod") DO (
-	start cmd.exe /c choco install -y %%a
-)
-
-::start cmd.exe /c choco install -y GoogleChrome & start cmd.exe /c choco install -y firefox & start cmd.exe /c choco install -y jdk7 & start cmd.exe /c choco install -y ie11 & start cmd.exe /c choco install -y startmenu8
+start cmd.exe /c choco install -y GoogleChrome & start cmd.exe /c choco install -y firefox & start cmd.exe /c choco install -y jdk7 & start cmd.exe /c choco install -y ie11 & start cmd.exe /c choco install -y startmenu8 & start cmd.exe /c choco install -y notepadplusplus & start cmd.exe /c choco install -y notepad2-mod
 
