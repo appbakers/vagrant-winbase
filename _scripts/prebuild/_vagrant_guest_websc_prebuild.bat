3481f11541a45d858de102ae393e980cdb335757
appbakers <greeny100@gmail.com>
2017-01-23 13:50:36 +0900

    * change folder structure to prebuild

---
+++ _scripts/prebuild/_vagrant_guest_websc_prebuild.bat
@@ -0,0 +1,9 @@
+###==> powershell with Admin right
+
+@call _vagrant_guest_inst_choco.bat
+
+@call _vagrant_guest_inst_vbox_guest_additions.ps1
+
+###==> installing GoogleChrome firefox jdk7 ie11
+start cmd.exe /c choco install -y wget & start cmd.exe /c choco install -y GoogleChrome & start cmd.exe /c choco install -y firefox & start cmd.exe /c choco install -y jdk7 & start cmd.exe /c choco install -y ie11 & start cmd.exe /c choco install -y startmenu8 & start cmd.exe /c choco install -y notepadplusplus & start cmd.exe /c choco install -y notepad2-mod
+
